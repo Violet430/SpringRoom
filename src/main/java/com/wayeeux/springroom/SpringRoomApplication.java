4559dc62f7425c7189c2a6a2b5fe9d44cb3f030d
Wayeeux <1525625981@qq.com>
2021-07-04 12:37:47 +0800

    Init Project

---
+++ src/main/java/com/wayeeux/springroom/SpringRoomApplication.java
@@ -0,0 +1,13 @@
+package com.wayeeux.springroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wayeeux/springroom/SpringRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayeeux.springroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
